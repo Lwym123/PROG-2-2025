2fc54e8dd871bffc6f9125c6591eda6d356b7b13
louwuyiming <1663859686@qq.com>
2026-03-17 16:11:48 +0800

    UserRegistration

---
+++  UserRegistration/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        UserRegistration userReg = new UserRegistration();
+        userReg.registration();
+        System.out.println(userReg);
+    }
+}
